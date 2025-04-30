99fed3072098dfae9c374369ddf0f03ad71f0531
Hans Rohit <hansrohit031@gmail.com>
2025-04-30 16:36:31 +0530

    redux-persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,17 @@ import "./assets/index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+let persistor = persistStore(store);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
 import studentReducer from "./slice/studentSlice";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  students: studentReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export const store = configureStore({
   devTools: true,
-  reducer: {
-    students: studentReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
